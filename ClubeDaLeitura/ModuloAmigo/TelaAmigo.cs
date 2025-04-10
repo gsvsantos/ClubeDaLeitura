95e6ad1c26c2792cdb3874353f75ba56e4e6b0f5
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-10 20:48:15 -0300

    feat: Adicionado construtor na classe `TelaAmigo.cs`.

--- ClubeDaLeitura/ModuloAmigo/TelaAmigo.cs
+++ ClubeDaLeitura/ModuloAmigo/TelaAmigo.cs
@@ -3,4 +3,9 @@
 public class TelaAmigo
 {
     public RepositorioAmigo RepositorioAmigo;
+
+    public TelaAmigo(RepositorioAmigo repositorioAmigo)
+    {
+        RepositorioAmigo = repositorioAmigo;
+    }
 }
