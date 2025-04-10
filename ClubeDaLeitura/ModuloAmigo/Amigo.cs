f09e991cab662532a68200fdb3162e81d856d05d
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-10 19:01:13 -0300

    feat: Adicionado construtor na classe `Amigo.cs`.

--- ClubeDaLeitura/ModuloAmigo/Amigo.cs
+++ ClubeDaLeitura/ModuloAmigo/Amigo.cs
@@ -8,4 +8,10 @@ public class Amigo : Entidade
     public string Nome;
     public string Telefone;
     private static int id = 0;
+    public Amigo(string nome, string responsavel, string telefone)
+    {
+        Nome = nome;
+        Responsavel = responsavel;
+        Telefone = telefone;
+    }
 }
