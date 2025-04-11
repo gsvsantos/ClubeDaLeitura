b9d5ef69f87c6467dce4098b1bd6eb78bdd21619
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-11 20:53:35 -0300

    feat: Adicionado construtor na classe `Emprestimo.cs`.

--- ClubeDaLeitura/ModuloEmprestimo/Emprestimo.cs
+++ ClubeDaLeitura/ModuloEmprestimo/Emprestimo.cs
@@ -1,13 +1,22 @@
-﻿using ClubeDaLeitura.ModuloAmigo;
+﻿using ClubeDaLeitura.Compartilhado;
+using ClubeDaLeitura.ModuloAmigo;
 using ClubeDaLeitura.ModuloRevista;
 
 namespace ClubeDaLeitura.ModuloEmprestimo;
 
-public class Emprestimo
+public class Emprestimo : Entidade
 {
     public Amigo Amigo;
     public Revista Revista;
     public DateTime Data;
     public string Situacao;
     private static int id = 0;
+
+    public Emprestimo(Amigo amigo, Revista revista, string situacao)
+    {
+        Amigo = amigo;
+        Revista = revista;
+        Data = DateTime.Now;
+        Situacao = situacao;
+    }
 }
