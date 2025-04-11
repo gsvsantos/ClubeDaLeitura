d147c829ad934cc33aacddac02da1508182ce281
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-11 20:52:42 -0300

    feat: Adicionado atributos na classe `Emprestimo.cs`.

--- ClubeDaLeitura/ModuloEmprestimo/Emprestimo.cs
+++ ClubeDaLeitura/ModuloEmprestimo/Emprestimo.cs
@@ -1,5 +1,13 @@
-﻿namespace ClubeDaLeitura.ModuloEmprestimo;
+﻿using ClubeDaLeitura.ModuloAmigo;
+using ClubeDaLeitura.ModuloRevista;
+
+namespace ClubeDaLeitura.ModuloEmprestimo;
 
 public class Emprestimo
 {
+    public Amigo Amigo;
+    public Revista Revista;
+    public DateTime Data;
+    public string Situacao;
+    private static int id = 0;
 }
