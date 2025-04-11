39748115d31cd90bf53db5653d0362aa6cd213d1
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-11 15:58:45 -0300

    feat: Adicionado atributos na classe `Caixa.cs`.

--- ClubeDaLeitura/ModuloCaixa/Caixa.cs
+++ ClubeDaLeitura/ModuloCaixa/Caixa.cs
@@ -1,5 +1,13 @@
-﻿namespace ClubeDaLeitura.ModuloCaixa;
+﻿using ClubeDaLeitura.Compartilhado;
+using ClubeDaLeitura.ModuloRevista;
 
-public class Caixa
+namespace ClubeDaLeitura.ModuloCaixa;
+
+public class Caixa : Entidade
 {
+    public string Etiqueta;
+    public string Cor;
+    public int DiasEmprestimo;
+    public Revista[] Revistas;
+    private static int id = 0;
 }
