2dd2aa19c69f660a0ab615813c00c05858855f61
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-12 16:04:59 -0300

    feat: Adicionado atributos na classe `Revista.cs`.

--- ClubeDaLeitura/ModuloRevista/Revista.cs
+++ ClubeDaLeitura/ModuloRevista/Revista.cs
@@ -1,5 +1,13 @@
-﻿namespace ClubeDaLeitura.ModuloRevista;
+﻿using ClubeDaLeitura.ModuloCaixa;
+
+namespace ClubeDaLeitura.ModuloRevista;
 
 public class Revista
 {
+    public string Titulo;
+    public int NumeroEdicao;
+    public int AnoPublicacao;
+    public string StatusEmprestimo;
+    public Caixa Caixa;
+    private static int id = 0;
 }
