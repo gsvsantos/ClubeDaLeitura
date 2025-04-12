e288e13c6da42bede5d80430bc494793a03a042f
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-11 22:58:41 -0300

    feat: Adicionado atributos na classe `TelaEmprestimo.cs`.

--- ClubeDaLeitura/ModuloEmprestimo/TelaEmprestimo.cs
+++ ClubeDaLeitura/ModuloEmprestimo/TelaEmprestimo.cs
@@ -1,5 +1,11 @@
-﻿namespace ClubeDaLeitura.ModuloEmprestimo;
+﻿using ClubeDaLeitura.ModuloAmigo;
+using ClubeDaLeitura.ModuloRevista;
+
+namespace ClubeDaLeitura.ModuloEmprestimo;
 
 public class TelaEmprestimo
 {
+    public RepositorioAmigo RepositorioAmigo;
+    public RepositorioRevista RepositorioRevista;
+    public RepositorioEmprestimo RepositorioEmprestimo;
 }
