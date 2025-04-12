e910f6b6d4cfd030f22f0575e1ac2bb170141f14
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-11 22:15:14 -0300

    feat: Adicionado atributo na classe `RepositorioEmprestimo.cs`.

--- ClubeDaLeitura/ModuloEmprestimo/RepositorioEmprestimo.cs
+++ ClubeDaLeitura/ModuloEmprestimo/RepositorioEmprestimo.cs
@@ -2,4 +2,7 @@
 
 public class RepositorioEmprestimo
 {
+    public Emprestimo[] Emprestimos = new Emprestimo[100];
+    public int IndiceListaEmprestimo = 0;
+    public bool ListaVazia = false;
 }
