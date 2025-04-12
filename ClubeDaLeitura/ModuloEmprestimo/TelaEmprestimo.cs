0a9a82656a04b272ef96b6e6130c758fd8ba78ed
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-11 23:02:29 -0300

    feat: Adicionado construtor na classe `TelaEmprestimo.cs`.

--- ClubeDaLeitura/ModuloEmprestimo/TelaEmprestimo.cs
+++ ClubeDaLeitura/ModuloEmprestimo/TelaEmprestimo.cs
@@ -8,4 +8,11 @@ public class TelaEmprestimo
     public RepositorioAmigo RepositorioAmigo;
     public RepositorioRevista RepositorioRevista;
     public RepositorioEmprestimo RepositorioEmprestimo;
+
+    public TelaEmprestimo(RepositorioEmprestimo repositorioEmprestimo, RepositorioAmigo repositorioAmigo, RepositorioRevista repositorioRevista)
+    {
+        RepositorioEmprestimo = repositorioEmprestimo;
+        RepositorioAmigo = repositorioAmigo;
+        RepositorioRevista = repositorioRevista;
+    }
 }
