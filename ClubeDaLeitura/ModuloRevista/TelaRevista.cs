6cab849c055d0119f4e8e7039e597adf9f949652
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-12 17:41:24 -0300

    feat: Adicionado construtor na classe `TelaRevista.cs`.

--- ClubeDaLeitura/ModuloRevista/TelaRevista.cs
+++ ClubeDaLeitura/ModuloRevista/TelaRevista.cs
@@ -6,4 +6,10 @@ public class TelaRevista
 {
     public RepositorioRevista RepositorioRevista;
     public RepositorioCaixa RepositorioCaixa;
+
+    public TelaRevista(RepositorioRevista repositorioRevista, RepositorioCaixa repositorioCaixa)
+    {
+        RepositorioRevista = repositorioRevista;
+        RepositorioCaixa = repositorioCaixa;
+    }
 }
