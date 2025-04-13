e5264b5f8f76ffde3b4088caf6c290cc9144f5da
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-13 18:00:39 -0300

    feat: Adicionado métodos estáticos na classe `ColorirEscrita.cs`.

--- ClubeDaLeitura/Compartilhado/ColorirEscrita.cs
+++ ClubeDaLeitura/Compartilhado/ColorirEscrita.cs
@@ -2,4 +2,16 @@
 
 public static class ColorirEscrita
 {
+    public static void SemQuebraLinha(string mensagem, ConsoleColor cor = ConsoleColor.White)
+    {
+        Console.ForegroundColor = cor;
+        Console.Write(mensagem);
+        Console.ResetColor();
+    }
+    public static void ComQuebraLinha(string mensagem, ConsoleColor cor = ConsoleColor.White)
+    {
+        Console.ForegroundColor = cor;
+        Console.WriteLine(mensagem);
+        Console.ResetColor();
+    }
 }
