ad638e38ce9e55facf6e696baf57814adfa7c2b7
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-15 13:24:03 -0300

    feat: Criada classe `Multa.cs` com atributos, construtor e métodos das multas.

---
+++ ClubeDaLeitura/ModuloMulta/Multa.cs
@@ -0,0 +1,41 @@
+﻿using ClubeDaLeitura.Compartilhado;
+using ClubeDaLeitura.ModuloEmprestimo;
+
+namespace ClubeDaLeitura.ModuloMulta;
+
+public class Multa : Entidade
+{
+    public Emprestimo Emprestimo;
+    public int DiasAtraso;
+    public double ValorMulta;
+    public string Status;
+    private static int id = 0;
+
+    public Multa(Emprestimo emprestimo)
+    {
+        Emprestimo = emprestimo;
+        DiasAtraso = CalcularDiasAtraso();
+        ValorMulta = CalcularValorMulta();
+        Status = "Pendente";
+    }
+    public void GerarId()
+    {
+        Id = ++id;
+    }
+    public double CalcularValorMulta()
+    {
+        return 2.0 * DiasAtraso;
+    }
+    public int CalcularDiasAtraso()
+    {
+        return DateTime.Now.Day - Emprestimo.ObterDataDevolucao().Day;
+    }
+    public void PagarMulta()
+    {
+        Status = "Quitada";
+    }
+    public bool EstaPendente()
+    {
+        return Status == "Pendente";
+    }
+}
